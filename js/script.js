8185c36f13214f922a024e288595c44844a27ef1
DanieleBaggiano <151195732+DanieleBaggiano@users.noreply.github.com>
2024-04-20 19:42:54 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,82 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            titleIcon: "&#10148;",
+            titlePrec: "TASK PRECEDENTI",
+            items: [
+                {
+                    icon: "&#10003;",
+                    text: "Quarterly Newsletter",
+                    cancel: "Editorial",
+                    color: "blu",
+                    done: true
+                },
+            ],
+            titleIconOggi: "&#10148;",
+            titleOggi: "OGGI",
+            itemsOggi: [
+                {
+                    icon: "&#10003;",
+                    text: "Recruiting blog post",
+                    cancel: "Editorial",
+                    color: "blu", 
+                    done: true
+                },
+                {
+                    icon: "&#10003;",
+                    text: "Mobile app launch",
+                    cancel: "Q1 Goals",
+                    color: "viola", 
+                    done: true
+                },
+                {
+                    icon: "&cross;",
+                    text: "Interview John H.",
+                    cancel: "Recruiting",
+                    color: "verde-acqua", 
+                    done: true
+                },
+                {
+                    icon: "&#10003;",
+                    text: "Summit update to mobile storefronts",
+                    cancel: "Mobile",
+                    color: "viola", 
+                    done: true
+                },
+            ],
+            titleIconPross: "&#10148;",
+            titlePross: "PROSSIMI GIORNI",
+            itemsPross: [
+                {
+                    icon: "&cross;",
+                    text: "schedule meeting with Alex",
+                    done: false
+                },
+                {
+                    icon: "&#10003;",
+                    text: "Homepage refresh",
+                    cancel: "Website",
+                    color: "rosa", 
+                    done: true
+                },
+                {
+                    icon: "&#10003;",
+                    text: "Onboard new Sales team members",
+                    cancel: "Sales",
+                    color: "verde", 
+                    done: true
+                },
+                {
+                    icon: "&#10003;",
+                    text: "Review editorial calendar",
+                    cancel: "Editorial",
+                    color: "blu", 
+                    done: true
+                },
+            ]
+        };
+    }
+}).mount("#app");
+
